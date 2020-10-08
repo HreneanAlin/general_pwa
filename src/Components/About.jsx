5c5f326f989ef863b500ca6415fd0b88643e1a25
HreneanAlin <hreneanalin@gmail.com>
2020-10-08 09:15:39 +0300

    cv added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import cvPDF from "../cv/CV_HreneanAlin_04102020.pdf"
 function About(props) {
     return (
         <main className="about-container">
             <h1>This site was made using React.js</h1>
+            <p>Check my CV <a href={cvPDF} without rel="noopener noreferrer" target="_blank">here</a></p>
         </main>
     );
 }
